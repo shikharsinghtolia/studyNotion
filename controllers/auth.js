08b9b538760347f88ebd5d929bd50e22b1aa75cb
Shikhar <Shikharsama@gmail.com>
2024-06-24 18:28:00 +0530

    Third Commit

---
+++ controllers/auth.js
@@ -0,0 +1,6 @@
+//signUP
+
+
+//Login
+
+
--- models/OTP.js
+++ models/OTP.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const mailSender = require("../utils/mailSender");
 const OTPSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -15,5 +15,20 @@ const OTPSchema = new mongoose.Schema({
     expires: 5 * 60,
   },
 });
+//function to send mails
+
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(email, "Verification Email", otp);
+    console.log("Email is this", mailResponse);
+  } catch (error) {
+    console.log("Sending email error is this:", error);
+  }
+}
+
+OTPSchema.pre("save",async function(next){
+  await sendVerificationEmail(this.email,this.otp);
+  next();
+})
 
 module.exports = mongoose.model("OTP", OTPSchema);
